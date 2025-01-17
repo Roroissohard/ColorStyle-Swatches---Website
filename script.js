b1488ffc7ffe85879227ed9fb1bfbb01abd40eab
Romier Manning <126rmanning@frhsd.com>
2025-01-17 12:53:47 -0500

    done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-    $("button").hover(
-        function() {
-            $(this).css("background-color", "yellow");
-        },
-        function() {
-            $(this).css("background-color", "");
-        }
-    );
-});
+function changeShoeColor(color) {
+    const shoeImage = document.getElementById('shoeImage');
+    const colorImages = {
+    black: 'img/new-balance-9060-black-castlerock-grey-sneakerhype-1.webp',
+    rain: 'img/new-balance-9060-rain-cloud-grey-sneakerhype-1_grande.webp',
+    sail: 'img/new-balance-9060-sail-grey-driftwood-sneakerhype-1.webp',
+};
+shoeImage.src = colorImages[color] || 'assets/img/placeholder.png';
+
+}
